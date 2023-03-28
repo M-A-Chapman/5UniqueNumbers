c567f9abed251d595a972589237dae0db5ae648e
Mike <64697794+M-Chapman@users.noreply.github.com>
2023-03-28 17:05:14 +0100

    added numbers list and sorting

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _5UniqueNumbers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>();
+
+        while (numbers.Count < 5)
+        {
+            Console.WriteLine("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (numbers.Contains(number))
+            {
+                System.Console.WriteLine("This number has already been entered, please enter a new number");
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+        };
+        numbers.Sort();
+        System.Console.WriteLine("The Sorted list of numbers is as follows:");
+        numbers.ForEach(Console.WriteLine);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # 5UniqueNumbers
 A simple C# program that asks for 5 numbers and if the number has already been input throws an error.
 Returns a sorted list of the numbers
+
+## Running
+```
+dotnet build
+dotnet run
+```
+## Process
+* Runs a for loop increasing a counter i from 0 to 10 in increments of 1
+* checks if the value of i is divisible by 2 with no remainder
+* if there is no remainder the value is output
+
+## Output
+```
+0
+2
+4
+6
+8
+10
+```
